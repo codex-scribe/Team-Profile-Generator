d9c5c70cb7656ca546e9d5ababfe1607050a778b
MrTompkinsGamow <mhbrandenburgh@gmail.com>
2023-02-06 18:32:58 -0500

    changed data handling

--- index.js
+++ index.js
@@ -1,35 +1,56 @@
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Employee = require('./lib/Employee');
-const inquirer = require('inquirer');
-const fs = require('fs');
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
+const inquirer = require("inquirer");
+const fs = require("fs");
+let employeeArray = [];
+let pageHTML = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+  integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+  crossorigin="anonymous"
+/>
+    </head>
+    <header>
+    <title>Team Profile Page</title>
+    <nav class="navbar navbar-emp">
+    <h5>Employee List</h5></nav> </header
+    <body>
+    <div class="row border-dark" id="card-area">`
 
 //Sets of questions for the inquirer prompts
 const managerQuestions = [
-    {
-        type: "input",
-        message: "Please enter the name of the manager:",
-        name: "name",
-      },
-      {
-        type: "input",
-        message: "Please enter the manager's employee id:",
-        name: "id",
-      },
-      {
-        type: "input",
-        message: "Please enter the manager's email:",
-        name: "email",
-      },
-      {
-        type: "input",
-        message: "Please enter the manager's office number:",
-        name: "officeNumber",
-      },
-]
+  {
+    type: "input",
+    message: "Please enter the name of the manager:",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "Please enter the manager's employee id:",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "Please enter the manager's email:",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "Please enter the manager's office number:",
+    name: "officeNumber",
+  },
+];
 
-const engineerQuestions =     [{
+const engineerQuestions = [
+  {
     type: "input",
     message: "Please enter the name of the engineer:",
     name: "name",
@@ -49,9 +70,10 @@ const engineerQuestions =     [{
     message: "Please enter the engineer's github username:",
     name: "github",
   },
-]
+];
 
-const internQuestions =     [{
+const internQuestions = [
+  {
     type: "input",
     message: "Please enter the name of the intern:",
     name: "name",
@@ -71,82 +93,82 @@ const internQuestions =     [{
     message: "Please enter the intern's school:",
     name: "school",
   },
-]
+];
 
 //logic to run when page initializes
 const init = () => {
-    fs.writeFile(
-        "./db/employees.json", '[]',
-        (err) => (err ? console.error(err) : console.log("success!"))
-      )
-      //Manager questions
-    inquirer.prompt(managerQuestions).then((data) => {
-        const manager = new Manager (data.name, data.id, data.email, data.officeNumber);
-        console.log(manager);
-        writeEmployees(manager);
-    }
-    )
-}
-
-
-
-//Writes data proved by user to a file
-const writeEmployees = (newEmployee) => {
-
-    let employeeList = fs.readFileSync("./db/employees.json");
-    let elArray = JSON.parse(employeeList);
-    elArray.push(newEmployee);
-    employeeList = JSON.stringify(elArray);
-    fs.writeFile(`./db/employees.json`, employeeList, (err) => err
-        ? console.error(err)
-        : console.log('New Employee has been written to JSON file!'))
+  //Manager questions
+  inquirer.prompt(managerQuestions).then((data) => {
+    const manager = new Manager(
+      data.name,
+      data.id,
+      data.email,
+      data.officeNumber
+    );
+    employeeArray.push(manager);
     addMoreOption();
-}
+  });
+};
 
 //asks if the user would like to add another employee
 const addMoreOption = () => {
-    inquirer.prompt({type: 'list', message: 'Do you want to add another employee?', choices: ['yes', 'no'], name: 'addAnother'})
-    .then((response) => {
-        if (response.addAnother == 'yes') {
-            addEmployee();
-        } else {
-            createPage();
-        }
+  inquirer
+    .prompt({
+      type: "list",
+      message: "Do you want to add another employee?",
+      choices: ["yes", "no"],
+      name: "addAnother",
     })
-}
+    .then((response) => {
+      if (response.addAnother == "yes") {
+        addEmployee();
+      } else {
+        createPage();
+      }
+    });
+};
 
 //
 const addEmployee = () => {
-    inquirer.prompt({type: 'list', message: 'What is this employee\'s role?', choices: ['Engineer', 'Intern'], name: 'newRole'})
-    .then((response) => {
-        switch (response.newRole) {
-            case 'Engineer':
-                engineerPrompts();
-                break;
-            case 'Intern':
-                internPrompts();
-                break;
-        }
-
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What is this employee's role?",
+      choices: ["Engineer", "Intern"],
+      name: "newRole",
     })
-}
+    .then((response) => {
+      switch (response.newRole) {
+        case "Engineer":
+          engineerPrompts();
+          break;
+        case "Intern":
+          internPrompts();
+          break;
+      }
+    });
+};
 
 const engineerPrompts = () => {
-    inquirer.prompt(engineerQuestions).then((data) => {
-        const engineer = new Engineer (data.name, data.id, data.email, data.github);
-        writeEmployees(engineer);
-    })
-}
+  inquirer.prompt(engineerQuestions).then((data) => {
+    const engineer = new Engineer(data.name, data.id, data.email, data.github);
+    employeeArray.push(engineer);
+    addMoreOption();
+  });
+};
 
 const internPrompts = () => {
-    inquirer.prompt(internQuestions).then((data) => {
-        const intern = new Intern (data.name, data.id, data.email, data.school);
-        writeEmployees(intern);
-    })
-}
+  inquirer.prompt(internQuestions).then((data) => {
+    const intern = new Intern(data.name, data.id, data.email, data.school);
+    employeeArray.push(intern);
+    addMoreOption();
+  });
+};
 
 const createPage = () => {
-    fs.writeFile(`index.html`, `
+  fs.writeFile(
+    `index.html`,
+    `
     <!DOCTYPE html>
     <html lang="en">
       <head>
@@ -177,9 +199,32 @@ const createPage = () => {
     crossorigin="anonymous"
   ></script>
 </html>
-    `,(err) => err
+    `,
+    (err) =>
+      err
         ? console.error(err)
-        : console.log('New Employee Team Page has been generated!'))
-}
+        : console.log("New Employee Team Page has been generated!")
+  );
+  let employeeList = fs.readFileSync("./db/employees.json");
+  let elArray = JSON.parse(employeeList);
+  console.log(employeeList);
+  console.log(elArray);
+  for (var i = 0; i < elArray.length; i++) {
+    var Employee = elArray[i];
+    if (Employee.officeNumber) {
+    //console.log(Employee);
+    var newCardHeader = document.createElement("div");
+    newCardHeader.className = "card text-white bg-secondary mb-3";
+    newCardHeader.setAttribute("style", "min-width: 20rem");
+    newCardHeader.innerHTML = `
+              <div class="card-header emp-head">${Employee.name} <br> Manager</div>
+              <div class="card-body emp-body">
+              <h4 class="card-text">Id: ${Employee.id}</h4>
+              <p>Days Available: ${Employee.daysAvail}</p>
+              </div> `;
+    cardArea.appendChild(newCardHeader);}
+
+  }
+};
 
 init();
